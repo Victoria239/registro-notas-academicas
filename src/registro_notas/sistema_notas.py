d26e85543a11ba2850126cb16d45c08bbc3f814a
Maria Victoria <mgiraldo3163@cue.edu.co>
2026-05-21 18:39:04 -0500

    feat: GREEN implementa estado de aprobación

--- src/registro_notas/sistema_notas.py
+++ src/registro_notas/sistema_notas.py
@@ -12,6 +12,14 @@ class SistemaNotas:
             "nota": nota
         })
 
+    def determinar_estado(self, nota):
+        self._validar_rango_nota(nota)
+
+        if nota >= 3.0:
+            return "Aprobado"
+
+        return "Reprobado"
+
     def _validar_rango_nota(self, nota):
         if nota < 0.0 or nota > 5.0:
             raise ValueError("La nota debe estar entre 0.0 y 5.0")
